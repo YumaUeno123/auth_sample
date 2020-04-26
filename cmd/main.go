a17bbac7a269e52af3cfe1a50c6768bb65baf338
ueno.yuma <ueno yuma>
2020-04-27 01:12:02 +0900

    Add router

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func signup(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("signup 関数実行")
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("login 関数実行")
+}
 
 func main() {
 	fmt.Println("Hello 世界")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+
+	log.Println("サーバー起動 : 8081 port で受信")
+
+	log.Fatal(http.ListenAndServe(":8081", router))
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/golangci/golangci-lint v1.25.0 // indirect
+	github.com/gorilla/mux v1.7.4 // indirect
 	github.com/swaggo/swag v1.6.5 // indirect
 	golang.org/x/tools v0.0.0-20200426102838-f3a5411a4c3b // indirect
 )
